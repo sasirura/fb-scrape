8ec47843aaab19cfae7d8bd4d87207765f5476a3
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-03-09 10:17:03 +0530

    initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+#import Facebook_scraper class from facebook_page_scraper
+from facebook_page_scraper import Facebook_scraper
+
+#instantiate the Facebook_scraper class
+
+page_name = "eminem"
+posts_count = 10
+browser = "firefox"
+proxy = "IP:PORT" #if proxy requires authentication then user:password@IP:PORT
+timeout = 600 #600 seconds
+headless = True
+meta_ai = Facebook_scraper(page_name, posts_count, browser, proxy=proxy, timeout=timeout, headless=headless)
+
+json_data = meta_ai.scrap_to_json()
+print(json_data)
+
+directory = "/home/sasiruravihansa/personal/fb-scrape"
+filename = "eminem"
+meta_ai.scrap_to_csv(filename, directory)
